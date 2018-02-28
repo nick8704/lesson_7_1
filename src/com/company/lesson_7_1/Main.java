9dae3dc85ba2237d5b33726943e963eed290950c
Alexandr Nikonets <nick8704@gmail.com>
2018-02-28 21:24:37 +0200

    data input

--- src/com/company/lesson_7_1/Main.java
+++ src/com/company/lesson_7_1/Main.java
@@ -1,8 +1,14 @@
 package com.company.lesson_7_1;
 
+import java.io.*;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static int even;
+    public static int odd;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        String inputString = input.readLine();
     }
 }
